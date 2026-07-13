dcb73c2f46d964a0500f2a2b74bc3aef40c3a349
Sammy <frnkverdam@gmail.com>
2026-07-13 21:13:10 +0200

    MovementScript(Update)

--- Assets/Scripts/Playable Character/Movement/PlayerMovement.cs
+++ Assets/Scripts/Playable Character/Movement/PlayerMovement.cs
@@ -1,43 +1,137 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private InputAction movementInput;
-    [SerializeField] private Transform GroundCheck;
-    private int MovementSpeed = 5;
-    private float JumpHeight = 1;
+    [Header("References")]
+    [SerializeField] private Transform playerCamera;
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -20f;
+
+    [Header("Camera")]
+    [SerializeField] private float mouseSensitivity = 0.15f;
+    [SerializeField] private float controllerSensitivity = 120f;
+
+    private CharacterController controller;
+
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+
+    private float verticalVelocity;
+    private float cameraPitch;
 
     private void Awake()
     {
+        controller = GetComponent<CharacterController>();
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // Movement
+        moveAction = new InputAction("Move", InputActionType.Value);
+
+        moveAction.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard>/s")
+            .With("Left", "<Keyboard>/a")
+            .With("Right", "<Keyboard>/d");
+
+        moveAction.AddBinding("<Gamepad>/leftStick");
+
+        // Mouse Look
+        lookAction = new InputAction("Look", InputActionType.Value);
+        lookAction.AddBinding("<Mouse>/delta");
+        lookAction.AddBinding("<Gamepad>/rightStick");
+
+        // Jump
+        jumpAction = new InputAction("Jump", InputActionType.Button);
+        jumpAction.AddBinding("<Keyboard>/space");
+        jumpAction.AddBinding("<Gamepad>/buttonSouth");
     }
+
     private void OnEnable()
     {
-        movementInput.Enable();
+        moveAction.Enable();
+        lookAction.Enable();
+        jumpAction.Enable();
     }
 
-    private void Walk()
+    private void OnDisable()
     {
-        Vector2 input = movementInput.ReadValue<Vector2>();
-
-        Vector3 walking = new Vector3(input.x, 5f, input.y);
-       
-        transform.Translate(walking * MovementSpeed * Time.deltaTime, Space.World);
-
-        Debug.Log("Input registered");
+        moveAction.Disable();
+        lookAction.Disable();
+        jumpAction.Disable();
     }
 
     private void Update()
     {
-        Walk();
+        Look();
+        Move();
     }
 
-
-    // Update is called once per frame
-    private void OnDisable()
+    private void Move()
     {
-        movementInput.Disable();
+        Vector2 input = moveAction.ReadValue<Vector2>();
+
+        Vector3 move =
+            transform.right * input.x +
+            transform.forward * input.y;
+
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (controller.isGrounded)
+        {
+            if (verticalVelocity < 0)
+                verticalVelocity = -2f;
+
+            if (jumpAction.triggered)
+            {
+                verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+        move.y = verticalVelocity;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    private void Look()
+    {
+        Vector2 lookInput = lookAction.ReadValue<Vector2>();
+
+        float lookX;
+        float lookY;
+
+        // Mouse input
+        if (Mouse.current != null && Mouse.current.delta.IsActuated())
+        {
+            lookX = lookInput.x * mouseSensitivity;
+            lookY = lookInput.y * mouseSensitivity;
+        }
+        // Controller right stick
+        else
+        {
+            lookX = lookInput.x * controllerSensitivity * Time.deltaTime;
+            lookY = lookInput.y * controllerSensitivity * Time.deltaTime;
+        }
+
+        cameraPitch -= lookY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
+        transform.Rotate(Vector3.up * lookX);
+    }
+
+    private void OnDestroy()
+    {
+        moveAction.Dispose();
+        lookAction.Dispose();
+        jumpAction.Dispose();
     }
 }
