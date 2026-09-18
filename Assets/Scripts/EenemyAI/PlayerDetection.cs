eaa9fa5cb98c3a218bc58094ab168ff0feddb023
Sammy <frnkverdam@gmail.com>
2026-09-18 09:56:49 +0200

    PlayerDetection

--- Assets/Scripts/EenemyAI/PlayerDetection.cs
+++ Assets/Scripts/EenemyAI/PlayerDetection.cs
@@ -12,17 +12,58 @@ public class PlayerDetection : MonoBehaviour
     [Header("Obstacles")]
     [SerializeField] private LayerMask obstacleLayer;
 
-    void Start()
+    private void Update()
     {
-        if (())
+        if (CanSeePlayer())
         {
-            Debug.Log("Can see player");
+            Debug.Log("Player detected!");
         }
     }
 
-    
-    void Update()
+    public bool CanSeePlayer()
     {
+        if (player == null)
+        {
+            return false;
+        }
+
+        Vector3 directionToPlayer = player.position - transform.position;
+        float distanceToPlayer = directionToPlayer.magnitude;
+
         
+        if (distanceToPlayer > detectionRange)
+        {
+            return false;
+        }
+
+        directionToPlayer.Normalize();
+
+        
+        float angleToPlayer = Vector3.Angle(
+            transform.forward,
+            directionToPlayer
+        );
+
+        if (angleToPlayer > fieldOfView / 2f)
+        {
+            return false;
+        }
+
+        
+        Vector3 eyePosition = transform.position + Vector3.up;
+
+        bool obstacleDetected = Physics.Raycast(
+            eyePosition,
+            directionToPlayer,
+            distanceToPlayer,
+            obstacleLayer
+        );
+
+        if (obstacleDetected)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
