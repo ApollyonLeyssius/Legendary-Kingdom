c009a0afe837acba28018ac08c3be9d5397bb8aa
Ace Brandligt <a.c.e.brandligt@gmail.com>
2026-09-18 12:41:08 +0200

    path tool
    verder uitgewerkt

--- Assets/Editor/PathEditor.cs
+++ Assets/Editor/PathEditor.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using UnityEditor;
 
-public class PathEditor : MonoBehaviour
+[CustomEditor(typeof(Path))]
+public class PathEditor : Editor
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Path path;
+
+    private void OnEnable()
     {
-        
+        path = (Path)target;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneGUI()
     {
-        
+        for (int i = 0; i < path.points.Length; i++)
+        {
+            Vector3 point = path.points[i];
+
+            EditorGUI.BeginChangeCheck();
+
+            Vector3 newPosition = Handles.PositionHandle(
+                point,
+                Quaternion.identity
+            );
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(path, "Move Path Point");
+
+                path.points[i] = newPosition;
+
+                EditorUtility.SetDirty(path);
+            }
+
+            Handles.Label(
+                point + Vector3.up * 0.5f,
+                "Point " + i
+            );
+
+            if (i > 0)
+            {
+                Vector3 previousPoint = path.points[i - 1];
+
+                Handles.DrawLine(
+                    previousPoint,
+                    point
+                );
+            }
+        }
     }
 }
